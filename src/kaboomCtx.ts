3c65fba9f4b8965a343f8fcf9bd93a147bd100bb
Connor Warme <connor.warme@gmail.com>
2024-05-04 11:06:17 -0600

    Initialize kaboom w/ scale workaround

---
+++ src/kaboomCtx.ts
@@ -0,0 +1,11 @@
+import kaboom from "kaboom";
+import { scale } from "./constants";
+
+export const k = kaboom({
+  // scale serves as workaround for kaboom issue where pixels aren't always sized properly/evenly
+  width: 256 * scale,
+  height: 144 * scale,
+  scale,
+  letterbox: true,
+  global: false
+});
